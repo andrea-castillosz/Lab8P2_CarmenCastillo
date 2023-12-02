2eba77ca8b4e48f73eb182d0e41c8353f9ae7d1a
andrea-castillosz <castillocandrea@gmail.com>
2023-12-02 00:55:34 -0600

    puta maje por que tuve que nacer tan lenta

--- src/Lab8P2_CarmenCastillo/HiloSimulacion.java
+++ src/Lab8P2_CarmenCastillo/HiloSimulacion.java
@@ -4,12 +4,54 @@
  */
 package Lab8P2_CarmenCastillo;
 
+import java.util.Random;
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author casti
  */
-public class HiloSimulacion {
+public class HiloSimulacion extends Thread{
     
+    private JProgressBar progBar_t;
+    private JProgressBar progBar_d;
+    private Random ran = new Random();
+    private double Tiempo2;
+    private double HPower;
+    private double VelocidadM;
+    private boolean avanzar;
+    private boolean vive;
+
+
+    public HiloSimulacion(JProgressBar progBar_t, JProgressBar progBar_d, double Tiempo2) {
+        this.progBar_t = progBar_t;
+        this.progBar_d = progBar_d;
+        this.Tiempo2 = Tiempo2;
+    }
+
+
+    public void setAvanzar(boolean avanzar) {
+        this.avanzar = avanzar;
+    }
+
+    public void setVive(boolean vive) {
+        this.vive = vive;
+    }
     
+    @Override
+    public void run() {
+        while (vive) {
+            if (avanzar) {
+//                progBar_d.setValue(Math.round(Integer.parseInt(sp_velocidad.getValue().toString()) * (progBar_t.getValue() / 60)));
+                progBar_d.setString(Integer.toString(progBar_d.getValue()) + " Kilometros/s");
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+
+            }
+        }
+
+    }
     
 }
